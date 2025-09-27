e8db0208a3c5d4298b33411dce1c3e007165ce15
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-09-27 11:50:12 +0700

    feat: set camera position

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+    GameObject hero;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hero = GameObject.Find("hero");
+
+        transform.rotation = hero.transform.rotation;
+
+        Vector3 posCam = hero.transform.localPosition;
+        posCam -= transform.forward * 5f;
+        transform.position = posCam;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(hero.transform);
     }
 }
