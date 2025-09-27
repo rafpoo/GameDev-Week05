377101f726b7e676ac5407445dae0fd9a519ab5f
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-09-27 12:01:19 +0700

    feat: move camera with keyboard

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     GameObject hero;
+
+    private float speed = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,30 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 50;
+        }
+        if (Input.GetKey(KeyCode.RightShift))
+        {
+            speed = -50;
+        }
         transform.LookAt(hero.transform);
+        transform.RotateAround(hero.transform.position, Vector3.up, speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            if (Vector3.Distance(transform.position, hero.transform.position) > 1f)
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime, Space.Self);
+            }
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            if (Vector3.Distance(transform.position, hero.transform.position) < 5f)
+            {
+                transform.Translate(Vector3.back * Time.deltaTime, Space.Self);
+            }
+        }
     }
 }
